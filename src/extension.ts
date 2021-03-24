2d0ccb394367a61561dfc3ef70461ac4be2608b6
Lior Bragilevsky <lbragile.masc@gmail.com>
2021-03-24 16:12:09 -0700

    added text to end of line

--- src/extension.ts
+++ src/extension.ts
@@ -4,11 +4,14 @@ export function activate(context: vscode.ExtensionContext) {
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "line-width-indicator" is now active!');
 
-  let disposable = vscode.commands.registerCommand("line-width-indicator.helloWorld", () => {
-    // The code you place here will be executed every time your command is executed
-
+  let disposable = vscode.commands.registerCommand("line-width-indicator.activateLineWidthIndicator", () => {
     // Display a message box to the user
-    vscode.window.showInformationMessage("Hello World from Line Width Indicator!");
+    vscode.window.showInformationMessage("Line Width Indicator is now active");
+
+    const editor = vscode.window.activeTextEditor;
+    const cursorPos = editor?.selection.active;
+    const lineEndPos = editor?.document.lineAt(cursorPos as vscode.Position).range.end;
+    editor?.edit((atCursorLineEnd) => atCursorLineEnd.insert(lineEndPos as vscode.Position, "hello"));
   });
 
   context.subscriptions.push(disposable);
